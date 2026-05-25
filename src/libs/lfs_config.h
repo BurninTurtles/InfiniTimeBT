43cf5adb0d3b70a7741fcaaaae0abaf6c718aacc
BurninTurtles <gyates100895@gmail.com>
2026-05-25 08:02:02 -0400

    Disable logging to enable building my rig

--- src/libs/lfs_config.h
+++ src/libs/lfs_config.h
@@ -2,25 +2,25 @@
 
 #include <libraries/log/nrf_log.h>
 
-#ifndef LFS_TRACE
-#ifdef LFS_YES_TRACE
-#define LFS_TRACE_(fmt, ...) \
-    NRF_LOG_DEBUG("[LFS] %s:%d:trace: " fmt "%s\n", __FILE__, __LINE__, __VA_ARGS__)
-#define LFS_TRACE(...) LFS_TRACE_(__VA_ARGS__, "")
-#else
-#define LFS_TRACE(...)
-#endif
-#endif
+//#ifndef LFS_TRACE
+//#ifdef LFS_YES_TRACE
+//#define LFS_TRACE_(fmt, ...) \
+//    NRF_LOG_DEBUG("[LFS] %s:%d:trace: " fmt "%s\n", __FILE__, __LINE__, __VA_ARGS__)
+//#define LFS_TRACE(...) LFS_TRACE_(__VA_ARGS__, "")
+//#else
+//#define LFS_TRACE(...)
+//#endif
+//#endif
 
-#ifndef LFS_DEBUG
-#ifndef LFS_NO_DEBUG
-#define LFS_DEBUG_(fmt, ...) \
-    NRF_LOG_DEBUG("[LFS] %s:%d:debug: " fmt "%s\n", __FILE__, __LINE__, __VA_ARGS__)
-#define LFS_DEBUG(...) LFS_DEBUG_(__VA_ARGS__, "")
-#else
-#define LFS_DEBUG(...)
-#endif
-#endif
+//#ifndef LFS_DEBUG
+//#ifndef LFS_NO_DEBUG
+//#define LFS_DEBUG_(fmt, ...) \
+//    NRF_LOG_DEBUG("[LFS] %s:%d:debug: " fmt "%s\n", __FILE__, __LINE__, __VA_ARGS__)
+//#define LFS_DEBUG(...) LFS_DEBUG_(__VA_ARGS__, "")
+//#else
+//#define LFS_DEBUG(...)
+//#endif
+//#endif
 
 #ifndef LFS_WARN
 #ifndef LFS_NO_WARN
